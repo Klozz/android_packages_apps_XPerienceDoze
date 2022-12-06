e76d744e9d47cfa5f776cd09b1e06fab2377b165
Bruno Martins <bgcngm@gmail.com>
2022-12-06 17:59:00 +0000

    doze: Fix compilation for QPR1 merge
    Change-Id: Ifa602ed1ae0734dbda40663770f2e54075bfa208

--- src/mx/xperience/doze/DozeSettingsActivity.java
+++ src/mx/xperience/doze/DozeSettingsActivity.java
@@ -22,7 +22,7 @@ package mx.xperience.doze;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.collapsingtoolbar.R;
+import com.android.settingslib.widget.R;
 
 public class DozeSettingsActivity extends CollapsingToolbarBaseActivity {
 
