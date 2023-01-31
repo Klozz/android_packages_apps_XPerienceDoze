e76ab4ef3e5eafef9aaf28d824776500041ac6fe
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-01-31 23:11:16 +0530

    Doze: Implement sleep for pickup gesture
    Put the device back to sleep when its lowered after being awoken
    by raise-to-wake on lockscreen. Stock ROMs do this wherein Xiaomi's
    pickup sensor reports value 2. This can be enabled by setting
    ro.sensor.pickup.lower.value (refer readme).
    
    Change-Id: I1a096389875116d8bd17198877d1f60a11f002d9
    Signed-off-by: klozz <carlosj@klozz.dev>

--- src/mx/xperience/doze/DozeService.java
+++ src/mx/xperience/doze/DozeService.java
@@ -43,6 +43,7 @@ public class DozeService extends Service {
         IntentFilter screenStateFilter = new IntentFilter();
         screenStateFilter.addAction(Intent.ACTION_SCREEN_ON);
         screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        screenStateFilter.addAction(Intent.ACTION_USER_PRESENT);
         registerReceiver(mScreenStateReceiver, screenStateFilter);
     }
 
@@ -68,9 +69,6 @@ public class DozeService extends Service {
 
     private void onDisplayOn() {
         if (DEBUG) Log.d(TAG, "Display on");
-        if (DozeUtils.isPickUpEnabled(this)) {
-            mPickupSensor.disable();
-        }
         if (DozeUtils.isHandwaveGestureEnabled(this) ||
                 DozeUtils.isPocketGestureEnabled(this)) {
             mProximitySensor.disable();
@@ -88,13 +86,26 @@ public class DozeService extends Service {
         }
     }
 
+    private void onUserPresent() {
+        if (DEBUG) Log.d(TAG, "User present");
+        if (DozeUtils.isPickUpEnabled(this)) {
+            mPickupSensor.disable();
+        }
+    }
+
     private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
-                onDisplayOn();
-            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
-                onDisplayOff();
+            switch (intent.getAction()) {
+                case Intent.ACTION_SCREEN_ON:
+                    onDisplayOn();
+                    break;
+                case Intent.ACTION_SCREEN_OFF:
+                    onDisplayOff();
+                    break;
+                case Intent.ACTION_USER_PRESENT:
+                    onUserPresent();
+                    break;
             }
         }
     };
